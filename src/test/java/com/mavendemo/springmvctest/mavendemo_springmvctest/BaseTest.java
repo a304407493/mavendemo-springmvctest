b0c6ff07b3fe6e94b2d35b9f7d17bc51542f8e56
Lenovo <675379272@qq.com>
2017-12-05 15:52:35 +0800

    测试使用的项目

--- src/test/java/com/mavendemo/springmvctest/mavendemo_springmvctest/BaseTest.java
+++ src/test/java/com/mavendemo/springmvctest/mavendemo_springmvctest/BaseTest.java
@@ -6,6 +6,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:applicationContext.xml"})
-public class BaseTest {
+public abstract class BaseTest {
 
 }
